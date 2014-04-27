379827df7969810f011cc71302123f1795a54e92
bootstraponline <code@bootstraponline.com>
2014-04-27 19:31:39 -0400

    Don't extend page

--- source/javascripts/app/toc.js
+++ source/javascripts/app/toc.js
@@ -10,7 +10,7 @@
   function toc () {
     toc = $("#toc").tocify({
       selectors: 'h1, h2',
-      extendPage: true,
+      extendPage: false,
       theme: 'none',
       smoothScroll: false,
       showEffectSpeed: 0,
